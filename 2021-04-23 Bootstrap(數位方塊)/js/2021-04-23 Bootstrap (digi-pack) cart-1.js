28e3312f7797e33e76b9da4f15e04508d69ec31c
GavinLin512 <81368692+GavinLin512@users.noreply.github.com>
2021-05-12 08:19:25 +0800

    2021-05-12 cart

--- 2021-04-23 Bootstrap(數位方塊)/js/2021-04-23 Bootstrap (digi-pack) cart-1.js
+++ 2021-04-23 Bootstrap(數位方塊)/js/2021-04-23 Bootstrap (digi-pack) cart-1.js
@@ -11,45 +11,62 @@ var totalCost = document.querySelector('.totalcost')
 var priceArr = document.querySelectorAll('.price')
 
 var priceNumberArr = [];
-var priceCountArr = [];
+var priceCountArr0 = [];
+var priceCountArr1 = [];
+var priceCountArr2 = [];
 
+var priceCountAll = [priceCountArr0,priceCountArr1,priceCountArr2]
+console.log(priceCountAll);
 
 
 
 // for迴圈，對應不同的商品
 for (let index = 0; index < count.length; index++) {
-// console.log(priceArr[index].dataset.price);
+    // console.log(priceArr[index].dataset.price);
 
     // 取得price的data
-    priceNumberArr.push(parseFloat(priceArr[index].dataset.price)) 
+    priceNumberArr.push(parseFloat(priceArr[index].dataset.price))
     // var priceNumber = parseFloat(price).toFixed(2)
     // console.log(amount.innerHTML);
     // console.log(priceNumberArr[index]);
 
-    
+
     // 數量增加
     plus[index].onclick = function () {
         // input跟數量顯示增加
         parseInt(count[index].value++)
         amount.innerHTML++
-        
-        // 小計價格
-        var priceCount = priceNumberArr[index] * parseInt(count[index].value)
-        
-        // priceCountArr.push(priceCount)
 
-        subTotal.innerHTML = '$' + priceCount.toFixed(2)
-        console.log(priceCount);
+        // 小計價格
+        // var priceCount = priceNumberArr[index] * parseInt(count[index].value)
+
+        for (let j = 0; j < priceCountAll.length; j++) {
+            priceCountAll[j].push(priceNumberArr[j] * parseInt(count[j].value))
+            
+        }
+
+        priceCountArr0.push(priceNumberArr[0] * parseInt(count[0].value))
+        priceCountArr1.push(priceNumberArr[1] * parseInt(count[1].value))
+        priceCountArr2.push(priceNumberArr[2] * parseInt(count[2].value))
+
+        var priceCount0 = priceCountArr0.pop()
+        var priceCount1 = priceCountArr1.pop()
+        var priceCount2 = priceCountArr2.pop()
+
+        var subTotalNumber = priceCount0 + priceCount1 + priceCount2
+
+        subTotal.innerHTML = '$' + subTotalNumber.toFixed(2)
+
         // 運費
         var shippingPrice = 24.90
         shipping.innerHTML = '$' + shippingPrice.toFixed(2)
-        
+
         // 總計價格
-        var totalCostNumber = parseFloat(parseFloat(priceCount.toFixed(2)) + shippingPrice).toFixed(2)
+        var totalCostNumber = parseFloat(parseFloat(subTotalNumber) + shippingPrice).toFixed(2)
         totalCost.innerHTML = '$' + totalCostNumber
 
-        
-        
+
+
 
         // console.log(typeof parseFloat(priceCount.toFixed(2)))
     }
@@ -65,24 +82,33 @@ for (let index = 0; index < count.length; index++) {
             amount.innerHTML--
 
             // 小計價格
-            var priceCount = priceNumberArr[index] * amount.innerHTML
-            subTotal.innerHTML = '$' + priceCount.toFixed(2)
+            priceCountArr0.push(priceNumberArr[0] * parseInt(count[0].value))
+            priceCountArr1.push(priceNumberArr[1] * parseInt(count[1].value))
+            priceCountArr2.push(priceNumberArr[2] * parseInt(count[2].value))
 
+            var priceCount0 = priceCountArr0.pop()
+            var priceCount1 = priceCountArr1.pop()
+            var priceCount2 = priceCountArr2.pop()
+            
+            var subTotalNumber = priceCount0 + priceCount1 + priceCount2
+
+            subTotal.innerHTML = '$' + subTotalNumber.toFixed(2)
+            // console.log(subTotal.innerHTML);
             // 運費
             var shippingPrice = 24.90
             shipping.innerHTML = '$' + shippingPrice.toFixed(2)
 
             // 總計價格
-            var totalCostNumber = parseFloat(parseFloat(priceCount.toFixed(2)) + shippingPrice).toFixed(2)
+            var totalCostNumber = parseFloat(parseFloat(subTotalNumber.toFixed(2)) + shippingPrice).toFixed(2)
             totalCost.innerHTML = '$' + totalCostNumber
-            
+
         }
 
         // 當數量等於0，或初始狀態
         if (amount.innerHTML == 0) {
-            
+
             // 小計、運費、總計為0
-            subTotal.innerHTML = '$' + priceCount.toFixed(0)
+            subTotal.innerHTML = '$' + subTotalNumber.toFixed(0)
             shipping.innerHTML = '$' + 0
             totalCost.innerHTML = '$' + 0
         }
